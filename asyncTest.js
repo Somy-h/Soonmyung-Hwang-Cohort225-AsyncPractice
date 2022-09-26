6723b1670d18ca473ee711ac5758c25839e041f1
Soonmyung Hwang <soonmyung.hwang.cohort225@gmail.com>
2022-09-26 16:09:16 -0700

    update more...

--- asyncTest.js
+++ asyncTest.js
@@ -2,7 +2,12 @@
 // Asynchronous code 
 // 1. callback
 //    - cause callback hell(pyramid of doom)
+// 	  - hard to handle errors
+//    - modern asynchronous APIs don't use callbacks
 // 2. Promise
+//    - foundation of asynchronous programming in modern JavaScript.
+//    - is an object returned by an asynchronous function
+//    - provides methods to handle the success or failure of the operation
 //    - promise chain
 // 3. async/await **
 //    - simpler way to work with asynchronous promise-based code
@@ -15,6 +20,7 @@
 
 //-------------------------------
 // fetch() API
+// : the modern, promise-based replacement for XMLHttpRequest
 //-------------------------------
 
 // const fetchPromise = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
@@ -29,7 +35,9 @@
 
 
 //-------------------------------
-// json() data
+// Chaining promises
+// fetch() & json() : asynchronous function, returns a promise
+// then() : Promise object provides then() method to handle success
 //-------------------------------
 
 // const fetchPromise = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
@@ -53,6 +61,7 @@
 
 //-------------------------------
 // Promise chain with error
+// Before trying to read it, check the status code in the response
 //-------------------------------
 // const fetchPromise = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
 
@@ -69,6 +78,7 @@
 
 //-------------------------------
 // Catching errors
+// catch() : Promise object provides catch() method to handle failure
 //-------------------------------
 // const fetchPromise = fetch('bad-scheme://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
 
@@ -88,16 +98,16 @@
 
 //-------------------------------
 // Promise terminology
-// *. pending: the promise has been created
-// *. fulfilled / rejected
+// * pending: the promise has been created
+// * fulfilled / rejected
 //    - fulfilled: the asynchronous function has succeeded.
 //                 then() handler is called
 //    - rejected: the asynchronous function has failed.
 //                 catch() handler is called
-// *. settled: fulfilled or rejected
-// *. resolved: either settled or following another promise 
+// * settled: fulfilled or rejected
+// * resolved: either settled or following another promise 
 //              that will determine what happens to this promise
-// *. unresolved: not resolved
+// * unresolved: not resolved
 //-------------------------------
 
 //-------------------------------
@@ -230,19 +240,183 @@
 	
 	//getCountriesInfo();
 
-
-
 //-------------------------------
 // Udemy: Error handling with fetch()
 //-------------------------------
 
-function handleErrors(res) {
-  if (!res.ok) throw new Error("HTTP ERROR: " + res.error);
-  return res;
+// function handleErrors(res) {
+//   if (!res.ok) throw new Error("HTTP ERROR: " + res.error);
+//   return res;
+// }
+
+// fetch('https://sdevcamper.io/api/v1/bootcamps/34343')
+// .then(res => res.json())
+// .then(handleErrors)
+// .then(res => console.log(res.data))
+// .catch(err => console.log("ERROR: " + err.message));
+
+
+//-------------------------------
+// Part 3 - Implementing a Promise Based API
+//-------------------------------
+// const output = document.querySelector('#output');
+// const button = document.querySelector('#set-alarm');
+
+// function setAlarm() {
+//   setTimeout(() => {
+//     output.textContent = 'Wake up!';
+//   }, 1000);
+// }
+
+// button.addEventListener('click', setAlarm);
+
+
+//-------------------------------
+// Part 3
+// promise-based alarm API, called alarm()
+// alarm() : return a Promise using Promise() constructor
+// Promise(executor) constructor:
+//    - executor: takes two arguments(resolve, reject)
+//    - succeeds, call the resolve
+//    - fails, call the reject
+//    - throws an error, reject is called automatically
+// alarm().then().catch()
+//-------------------------------
+// const name = document.querySelector('#name');
+// const delay = document.querySelector('#delay');
+// const button = document.querySelector('#set-alarm');
+// const output = document.querySelector('#output');
+
+
+// function alarm(person, delay) {
+// 	return new Promise((resolve, reject) => {
+// 		if (delay < 0) {
+// 			throw new Error('Alarm delay must not be negative');
+// 		}
+
+// 		setTimeout(() => {
+// 			resolve(`Wake up, ${person}`);
+// 		}, delay);
+// 	});
+// }
+
+
+
+// button.addEventListener('click', () => {
+// 	alarm(name.value, delay.value)
+// 		.then((message) => output.textContent = message)
+// 		.catch((error) => output.textContent = `Couldn't set alarm: ${error}`);
+// });
+
+
+//-------------------------------
+// Part 3
+// Using async and await with the alarm() API
+// Don't forget to use async function
+//-------------------------------
+// const name = document.querySelector('#name');
+// const delay = document.querySelector('#delay');
+// const button = document.querySelector('#set-alarm');
+// const output = document.querySelector('#output');
+
+
+// function alarm(person, delay) {
+// 	return new Promise((resolve, reject) => {
+// 		if (delay < 0) {
+// 			throw new Error('Alarm delay must not be negative');
+// 		}
+
+// 		setTimeout(() => {
+// 			resolve(`Wake up, ${person}`);
+// 		}, delay);
+// 	});
+// }
+
+
+
+// button.addEventListener('click', async () => {
+// 	// alarm(name.value, delay.value)
+// 	// 	.then((message) => output.textContent = message)
+// 	// 	.catch((error) => output.textContent = `Couldn't set alarm: ${error}`);
+
+// 	try {
+// 		const message = await alarm(name.value, delay.value);
+// 		output.textContent = message;
+// 	}
+// 	catch (error) {
+// 		output.textContent = `Couldn't set alarm: ${error}`;
+// 	}
+
+// });
+
+
+//-------------------------------
+// Part 4 - Introducing workers
+// Workers: give you the ability to run some tasks in a different thread
+//   - Workers and the main code run in completely separate worlds
+//     so, both threads cannot access to the same variables
+//     only interact by sending each other messages
+// *** workers can't access the DOM ***
+// 3 kinds of workers
+//   - dedicated workers
+//   - shared workers
+//      : can be shared by several different scripts running in different windows
+//   - service workers
+//      : caching resources so that web applications can work when the user is offline.
+//-------------------------------
+
+
+//-------------------------------
+// Part 4 - Implementing dedicated worker (gnerate Primes)
+// - communicate with messages
+//   work.postMessage() ***
+//   work.addEventListener('message', {...message.data...}) ***
+//-------------------------------
+// Create a new worker, giving it the code in "generate.js"
+// const worker = new Worker('./generate.js');
+
+// document.querySelector('#generate').addEventListener('click', () => {
+//   const quota = document.querySelector('#quota').value;
+//   worker.postMessage({
+//     command: 'generate',
+//     quota,
+//   });
+// });
+
+// worker.addEventListener('message', (message) => {
+//   document.querySelector('#output').textContent = `Finished generating ${message.data} primes!`;
+// });
+
+// document.querySelector('#reload').addEventListener('click', () => {
+//   document.querySelector('#user-input').value = 'Try typing in here immediately after pressing "Generate primes"';
+//   document.location.reload();
+// });
+
+
+//-------------------------------
+// Part 5 - Sequencing animations
+// - Web Animations API : let us construct animations and control their playback with JavaScript
+// - element.animate()
+//-------------------------------
+const aliceTumbling = [
+  { transform: 'rotate(0) scale(1)' },
+  { transform: 'rotate(360deg) scale(0)' }
+];
+
+const aliceTiming = {
+  duration: 2000,
+  iterations: 1,
+  fill: 'forwards'
 }
 
-fetch('https://sdevcamper.io/api/v1/bootcamps/34343')
-.then(res => res.json())
-.then(handleErrors)
-.then(res => console.log(res.data))
-.catch(err => console.log("ERROR: " + err.message));
+const alice1 = document.querySelector("#alice1");
+const alice2 = document.querySelector("#alice2");
+const alice3 = document.querySelector("#alice3");
+
+
+alice1.animate(aliceTumbling, aliceTiming).finished
+  .then(() => alice2.animate(aliceTumbling, aliceTiming).finished)
+  .then(() => alice3.animate(aliceTumbling, aliceTiming).finished)
+  .catch(error => console.error(`Error animating Alices: ${error}`));
+
+
